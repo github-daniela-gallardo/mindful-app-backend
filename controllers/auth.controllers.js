a199af7ac4e1fcd032bcfc0b25abb935b19e81c3
Daniela Gallardo <promillennials@gmail.com>
2022-12-16 17:33:54 -0500

    route auth done

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,11 +1,11 @@
 const bcryptjs = require('bcryptjs');
-const User =require('../models/User.model');
+const User = require('../models/User.model');
 const jwt = require('jsonwebtoken');
 
-const salt =10;
+const salt = 10;
 
 const signUpControllers = (req, res, next) => {
-    if (!req.body.email || req.body.password || req.body.userName){
+    if (!req.body.email || !req.body.password || !req.body.userName) {
         return res.status(400).json({
             error: {
                 message: 'All fields are require'
@@ -18,10 +18,55 @@ const signUpControllers = (req, res, next) => {
         password: bcryptjs.hashSync(req.body.password, salt),
         userName: req.body.userName
     })
-    .then(createdUser => {
-        console.log('this is the new user', createdUser)
-    })
+        .then(createdUser => {
+            console.log('this is the new user', createdUser)
+        })
+        .catch(err => console.log(err))
 
-  }
+}
 
-  module.exports = {signUpControllers }
+const loginController = (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.json({
+            error: {
+                massage: 'Missing email or password'
+            }
+        })
+    }
+
+    let myUser;
+    User.findOne({ email})
+        .then(foundUser => {
+            if (!foundUser) {
+                return Promise.reject('Invalid email or password')
+            }
+            myUser = foundUser
+            return bcryptjs.compare(password, foundUser.password)
+                
+        }).then(isValidPassword => {
+                    if (!isValidPassword) {
+                        return Promise.reject('Invalid email or password')
+                    }
+
+                    const payload = {
+                        _id: myUser._id,
+                        email: myUser.email,
+                        userName: myUser.userName
+                    }
+                    const authToken = jwt.sign(
+                        payload,
+                        process.env.TOKEN_SECRET,
+                        { algorithm: 'HS256', expiresIn: "12h" }
+                    );
+                    res.json({
+                        authToken: authToken
+                    })
+                })
+        .catch(err => console.log(err))
+}
+
+
+
+module.exports = { signUpControllers, loginController }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,18 @@
 let express = require('express');
 let router = express.Router();
 
-const {signUpControllers} = require('../controllers/auth.controllers')
+const {signUpControllers, loginController} = require('../controllers/auth.controllers')
+const {isAuthenticated} = require ('../middleware/jwt.middleware')
 
-router.post('/', signUpControllers);
+
+router.post('/signup', signUpControllers);
+
+router.post('/login', loginController );
+
+router.get('/verify', isAuthenticated , (req, res, next) => {
+    console.log(req.payload);
+    res.json(req.payload)
+})
+
 
 module.exports = router;
